0eec5213c7808a5e311ce9299f73bfe10b5b9c4e
Jie Zheng <Jayie21@live.com>
2019-05-03 15:08:59 -0500

    Long time update

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -1,8 +1,22 @@
-import java.io.IOException;
+import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.exporter.HTTPServer;
+import io.prometheus.client.hotspot.DefaultExports;
 
 public class MainController {
 
-    public static void main(String[] args) throws IOException{
+    private static final Gauge JOBS_IN_QUEUE = Gauge.build()
+            .name("jobs_in_queue")
+            .help("Current number of jobs in the queue")
+            .register();
+
+    private static final Counter myCounter = Counter.build()
+            .name("my_counter_total")
+            .help("An example counter.").register();
+
+
+
+    public static void main(String[] args) throws Exception{
 
         if(args.length != 2){
             System.out.println("{url} {authorizationCode}");
@@ -13,7 +27,22 @@ public class MainController {
         String authorizationCode = args[1];
 
         Board board = new Board(url, authorizationCode);
+        DefaultExports.initialize();
         Metric metric = new Metric(board);
+
+
+    }
+
+    private static void initializerPrometheus() throws Exception{
+        HTTPServer prometheusServer = new HTTPServer(7080);
+    }
+
+    public void enqueueJob(){
+        JOBS_IN_QUEUE.inc();
+    }
+
+    public void runNextJob(){
+        JOBS_IN_QUEUE.dec();
     }
 
 }
--- src/main/java/Metric.java
+++ src/main/java/Metric.java
@@ -1,20 +1,46 @@
+import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.exporter.HTTPServer;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Metric {
 
     private Board board;
-    private HashMap<String, Integer> eachUserWorload;
+    private HashMap<String, ArrayList<Card>> data;
+    private HashMap<String, Integer> eachUserWorkload;
+    private Date currentLeadTime;
+    private Date startTime;
+    private ArrayList<String> lanes;
+    private HashMap<String, Gauge> gauges_cycleTimes = new HashMap<String, Gauge>();
+    private HashMap<String, Gauge> gauges_eachUserWorkLoad = new HashMap<String, Gauge>();
+
+    private static final Counter myCounter = Counter.build()
+            .name("team_productivity")
+            .help("It shows the change of total size of cards.").register();
+    private HTTPServer server;
 
     public Metric(Board board) throws IOException{
         this.board = board;
-        refresh();
+        server = new HTTPServer(2020);
+        data = board.getData();
+        lanes = new ArrayList<String>();
+        lanes.addAll(Arrays.asList("Planned", "ACTIVE_DEVELOPMENT", "CODE_REVIEW",
+                "DEV_COMPLETE", "AVAILABLE_FOR_TESTING", "TESTING", "PASSED_QA", "ARTIFACT_CREATED"));
+        update();
+
     }
 
-    public void refresh() throws IOException {
+    public void update() throws IOException {
         board.refresh();
+        data = board.getData();
         getEachUserWorkload();
+        setGauges_eachUserWorkLoad();
     }
 
     private void getEachUserWorkload(){
@@ -28,18 +54,69 @@ public class Metric {
                 if(!card.getAssignedUser().isEmpty()){
                     for(String userName : card.getAssignedUser()){
                         if(!userWorkload.containsKey(userName)){
-                            userWorkload.put(userName, card.getSize());
+                            userWorkload.put(trimName(userName), card.getSize());
                         }
-                        userWorkload.put(userName, userWorkload.get(userName) + card.getSize());
+                        userWorkload.put(trimName(userName), userWorkload.get(trimName(userName)) + card.getSize());
                     }
                 }
             }
         }
 
-        eachUserWorload = userWorkload;
+        eachUserWorkload = userWorkload;
+    }
+
+    private void refreshMetrics(){
+
+    }
+
+    private ArrayList<Card> getCardsFromLane(String lane) throws IOException{
+        board.refresh();
+        HashMap<String, ArrayList<Card>> data = board.getData();
+        return data.get(lane);
+    }
+
+    private void getLeadTime() {
+    }
+
+    private void getCycleTimes(){
+        Date newLeadTime = new Date();
+        Date currentTime = new Date();
+        setLanes(lanes, data);
+        for(String lane : data.keySet()){
+
+        }
+    }
+
+    private HashMap<String, Card> setLanes(ArrayList<String> lanes, HashMap<String, ArrayList<Card>> data){
+        for(String lane : data.keySet()){
+            if(!lanes.contains(lane)){
+                data.remove(lane);
+            }
+        }
+
+        throw new NotImplementedException();
     }
 
     public HashMap<String, Integer> getEachUserWorload() {
-        return eachUserWorload;
+        return eachUserWorkload;
+    }
+
+    private void setGauges_eachUserWorkLoad(){
+        for(String workerName : eachUserWorkload.keySet()){
+            Gauge newgauge = Gauge.build().name(workerName).help("Worker's workload").register();
+            newgauge.set(eachUserWorkload.get(trimName(workerName)));
+            gauges_eachUserWorkLoad.put(workerName, newgauge);
+        }
+    }
+
+    private String trimName(String name){
+        char[] chAry = name.toCharArray();
+        for(int i = 0; i < chAry.length; i++){
+            String ch = Character.toString(chAry[i]);
+            if(ch.equals(" ")){
+                chAry[i] = '_';
+            }
+        }
+        return new String(chAry);
     }
 }
